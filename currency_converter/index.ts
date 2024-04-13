f81e9afe9c058f6f16ba4f353559feb7d18ef9ed
Owasis-tech55 <160662560+Owasis-tech55@users.noreply.github.com>
2024-04-13 12:46:15 +0500

    Add files via upload

---
+++ currency_converter/index.ts
@@ -0,0 +1,44 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk"
+let currency:any = {
+  USD: 1,
+  PKR: 277.83,
+  EUR: 0.94,
+  GBP: 0.80,
+  INR: 83.62,
+  SAR: 3.75,
+  AED: 3.67,
+  KWD: 0.31,
+  JPY: 153.01,
+  CNY: 7.24, 
+};
+
+let input = await inquirer.prompt([
+  {
+    name: "from",
+    type: "list",
+    message: "Select your currency",
+    choices: ["USD", "EUR", "GBP", "INR", "PKR", "SAR", "AED", "KWD", "JPY", "CNY"],
+  },
+  {
+    name: "to",
+    type: "list",
+    message: "Select your currency",
+    choices: ["USD", "EUR", "GBP", "INR", "PKR", "SAR", "AED", "KWD", "JPY", "CNY"],
+  },
+  {
+    name: "amount",
+    type: "input",
+    message: "Enter your amount to convert",
+  },
+]);
+
+
+let fromcurrency = currency[input.from]
+let tocurrency = currency[input.to]
+let amount = input.amount
+let basecurrency = amount / fromcurrency
+let convertedcurrency = basecurrency * tocurrency
+console.log(chalk.bold.magenta(convertedcurrency.toFixed(3)));
